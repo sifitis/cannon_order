0fd305da764552649ad43c6acc9fbf23110dc7fd
sifitis <nucleartoast5120@gmail.com>
2026-03-24 20:00:24 -0400

    Initial commit

---
+++ cannon_order.lua
@@ -0,0 +1,34 @@
+
+br = peripheral.find("block_reader")
+
+term.clear()
+term.setCursorPos(1,1)
+
+-- Helper functions --
+
+function newLine()
+	local x,y = term.getCursorPos()
+	term.setCursorPos(1,y+1)
+end
+
+function parseClipboard(slot)
+	local out = {}
+	local num = 0
+	local cbc = br.getBlockData().Items[1].components["create:clipboard_content"]
+	for i,page in ipairs(cbc.pages) do
+		for j,entry in ipairs(page) do
+			num = num + 1
+			local count = entry.item_amount
+			local item = entry.icon.id
+			out[num] = {item=item,count=count}
+		end
+	end
+	return out
+end
+
+function printList(list)
+	for i,entry in ipairs(list) do
+		print(entry[item].." - "entry[count])
+		newLine()
+	end
+end
